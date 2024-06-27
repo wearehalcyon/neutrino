10efe7ce442f1bdb9a9f22af8e9c96ee4844eaca
Roman A <thewitness45@gmail.com>
2024-06-27 15:01:20 +0300

    add dash sources

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -9,6 +9,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-
+        return view('dashboard.page-index');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('front.layouts.app')
 
 @section('content')
 <div class="container">
