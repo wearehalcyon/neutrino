c06df407383d450eaecd2c3dd82d73dab3b6d092
Roman A <thewitness45@gmail.com>
2024-06-29 13:11:23 +0300

    add posts

--- resources/views/dashboard/page-fm.blade.php
+++ resources/views/dashboard/page-fm.blade.php
@@ -3,6 +3,8 @@
 @section('title', __('Media Files'))
 
 @section('header-scripts')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 @endsection
 
