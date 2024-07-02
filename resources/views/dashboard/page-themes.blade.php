58fbcf6cf4c87923b2f5ad7a8d65d025866091ff
Roman A <thewitness45@gmail.com>
2024-07-02 18:15:59 +0300

    init front

--- resources/views/dashboard/page-themes.blade.php
+++ resources/views/dashboard/page-themes.blade.php
@@ -26,7 +26,7 @@
             line-height: 18px;
         }
         .theme-info-full{
-            display: flex;
+            display: none;
             flex-wrap: wrap;
             align-items: center;
             justify-content: center;
@@ -38,18 +38,21 @@
             background-color: rgba(0,0,0,.4);
             z-index: 9999;
         }
+        .theme-info-full.show{
+            display: flex;
+        }
         .theme-info-full .window-content{
-            display: none;
+            display: flex;
             flex-wrap: wrap;
             background-color: #fff;
             border-radius: 10px;
-        }
-        .theme-info-full .window-content.show{
-            display: flex;
+            width: 1200px;
+            max-width: 100%;
         }
         .theme-info-full .window-content .column{
             width: 50%;
             display: block;
+            position: relative;
         }
         @media screen and (max-width: 992px) {
             .theme-info-full .window-content .column{
@@ -59,6 +62,27 @@
         .theme-info-full .window-content .column img{
             width: 100%;
             height: auto;
+        }
+        .close-window-content{
+            border: none;
+            width: 32px;
+            height: 32px;
+            display: inline-flex;
+            flex-wrap: wrap;
+            align-items: center;
+            justify-content: center;
+            font-size: 18px;
+            line-height: 18px;
+            background-color: rgba(0,0,0,.05);
+            position: absolute;
+            top: 0px;
+            right: 0px;
+            border-radius: 100px;
+            font-weight: 100;
+            z-index: 10;
+        }
+        .close-window-content:hover{
+            background-color: rgba(0,0,0,.1);
         }
     </style>
 @endsection
@@ -118,19 +142,64 @@
                             if (File::exists($information)) {
                                 (array)$infoData = json_decode(File::get($information));
                             } else {
-                                $infoData = '';
+                                $infoData = [];
                             }
                         @endphp
-                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-3 mt-3">
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-3 mt-2 mb-2">
                             <div class="card">
                                 <div class="card-body">
-                                    <img class="screenshot" src="{{ $base64Image }}" alt="{{ $infoData->name . ' Screenshot Preview' }}">
-                                    <h4 class="mt-3"><strong>{{ $infoData->name }}</strong></h4>
-                                    <small class="form-text text-muted"><strong>{{ __('Version: ') }}</strong>{{ $infoData->version }}</small>
+                                    @if($base64Image != "")
+                                        <img class="screenshot" src="{{ $base64Image }}" alt="{{ optional($infoData)->name . ' Screenshot Preview' }}">
+                                    @else
+                                        <img class="screenshot" src="{{ asset('assets/images/no-thumbnail.jpg') }}" alt="{{ optional($infoData)->name . ' Screenshot Preview' }}">
+                                    @endif
+                                    <h4 class="mt-3">
+                                        <strong>
+                                            @if(optional($infoData)->name)
+                                                {{ optional($infoData)->name }}
+                                            @else
+                                                {{ $themeActive->option_value }}
+                                            @endif
+                                        </strong>
+                                    </h4>
+                                    @if(!is_array($infoData))
+                                        <small class="form-text text-muted"><strong>{{ __('Version: ') }}</strong>{{ optional($infoData)->version }}</small>
+                                    @else
+                                        <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert" style="box-shadow: none; background-color: #ffe1e3; font-size: 12px; line-height: 16px;">
+                                            {{ __('The information file was not found or was corrupted. The current theme may not work stably. Make sure the theme files are present and not damaged.') }}
+                                        </div>
+                                    @endif
                                 </div>
                                 <div class="card-footer">
                                     <div class="buttons px-1 py-2">
-                                        <a href="javascript:;" class="btn btn-primary no-filter">{{ __('Information') }}</a>
+                                        @if(!is_array($infoData))
+                                            <a href="javascript:;" class="btn btn-primary no-filter open-full-info-popup" data-theme="{{ $themeActive->option_value }}">{{ __('Information') }}</a>
+                                        @endif
+                                        <div id="{{ $themeActive->option_value }}" class="theme-info-full">
+                                            <div class="card m-4">
+                                                <div class="card-body">
+                                                    <div class="window-content">
+                                                        <div class="column p-2 p-lg-3">
+                                                            @if($base64Image != "")
+                                                                <img src="{{ $base64Image }}" alt="{{ optional($infoData)->name . ' Screenshot Full Preview' }}">
+                                                            @else
+                                                                <img src="{{ asset('assets/images/no-thumbnail.jpg') }}" alt="{{ optional($infoData)->name . ' Screenshot Full Preview' }}">
+                                                            @endif
+                                                        </div>
+                                                        <div class="column p-2 p-lg-3" style="border-left: 1px solid rgba(0,0,0,.1);">
+                                                            <button type="button" class="close-window-content page-link">✕</button>
+                                                            <h2>{{ optional($infoData)->name }}</h2>
+                                                            <hr style="opacity: .15;">
+                                                            <p style="margin-bottom: 0;"><strong>{{ __('Author: ') }}</strong><a href="{{ optional($infoData)->authorUri }}" target="_blank">{{ optional($infoData)->author }}</a></p>
+                                                            <p style="margin-bottom: 0;"><strong>{{ __('Theme URI: ') }}</strong><a href="{{ optional($infoData)->themeUri }}" target="_blank">{{ optional($infoData)->themeUri }}</a></p>
+                                                            <p style="margin-bottom: 0;"><strong>{{ __('Version: ') }}</strong>{{ optional($infoData)->version }}</p>
+                                                            <p style="margin-bottom: 0;"><strong>{{ __('Description: ') }}</strong>{{ optional($infoData)->description }}</p>
+                                                            <p style="margin-bottom: 0;"><strong>{{ __('License: ') }}</strong>{{ optional($infoData)->license }}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -148,20 +217,40 @@
                                 if (File::exists($information)) {
                                     (array)$infoData = json_decode(File::get($information));
                                 } else {
-                                    $infoData = '';
+                                    $infoData = [];
                                 }
                             @endphp
                             @if($theme != $themeActive->option_value)
-                                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-3 mt-3">
+                                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-3 mt-2 mb-2">
                                     <div class="card">
                                         <div class="card-body">
-                                            <img class="screenshot" src="{{ $base64Image }}" alt="{{ $infoData->name . ' Screenshot Preview' }}">
-                                            <h4 class="mt-3"><strong>{{ $infoData->name }}</strong></h4>
-                                            <small class="form-text text-muted"><strong>{{ __('Version: ') }}</strong>{{ $infoData->version }}</small>
+                                            @if($base64Image != "")
+                                                <img class="screenshot" src="{{ $base64Image }}" alt="{{ optional($infoData)->name . ' Screenshot Preview' }}">
+                                            @else
+                                                <img class="screenshot" src="{{ asset('assets/images/no-thumbnail.jpg') }}" alt="{{ optional($infoData)->name . ' Screenshot Preview' }}">
+                                            @endif
+                                            <h4 class="mt-3">
+                                                <strong>
+                                                    @if(optional($infoData)->name)
+                                                        {{ optional($infoData)->name }}
+                                                    @else
+                                                        {{ $theme }}
+                                                    @endif
+                                                </strong>
+                                            </h4>
+                                            @if(!is_array($infoData))
+                                                <small class="form-text text-muted"><strong>{{ __('Version: ') }}</strong>{{ optional($infoData)->version }}</small>
+                                            @else
+                                                <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert" style="box-shadow: none; background-color: #ffe1e3; font-size: 12px; line-height: 16px;">
+                                                    {{ __('The information file was not found or was corrupted. The current theme may not work stably. Make sure the theme files are present and not damaged.') }}
+                                                </div>
+                                            @endif
                                         </div>
                                         <div class="card-footer">
                                             <div class="buttons px-1 py-2">
-                                                <a href="javascript:;" class="btn btn-primary no-filter" data-theme="{{ $theme }}">{{ __('Information') }}</a>
+                                                @if(!is_array($infoData))
+                                                    <a href="javascript:;" class="btn btn-primary no-filter open-full-info-popup" data-theme="{{ $theme }}">{{ __('Information') }}</a>
+                                                @endif
                                                 @if($theme != $themeActive->option_value)
                                                     <form class="d-inline-block w-auto" action="{{ route('dash.themes.activate', $theme) }}" method="post">
                                                         @csrf
@@ -177,26 +266,33 @@
                                                         <div class="card-body">
                                                             <div class="window-content">
                                                                 <div class="column p-2 p-lg-3">
-                                                                    <img src="{{ $base64Image }}" alt="">
+                                                                    @if($base64Image != "")
+                                                                        <img src="{{ $base64Image }}" alt="{{ optional($infoData)->name . ' Screenshot Full Preview' }}">
+                                                                    @else
+                                                                        <img src="{{ asset('assets/images/no-thumbnail.jpg') }}" alt="{{ optional($infoData)->name . ' Screenshot Full Preview' }}">
+                                                                    @endif
                                                                 </div>
                                                                 <div class="column p-2 p-lg-3" style="border-left: 1px solid rgba(0,0,0,.1);">
-                                                                    <h2>{{ $infoData->name }}</h2>
+                                                                    <button type="button" class="close-window-content page-link">✕</button>
+                                                                    <h2>{{ optional($infoData)->name }}</h2>
+                                                                    <hr style="opacity: .15;">
+                                                                    <p style="margin-bottom: 0;"><strong>{{ __('Author: ') }}</strong><a href="{{ optional($infoData)->authorUri }}" target="_blank">{{ optional($infoData)->author }}</a></p>
+                                                                    <p style="margin-bottom: 0;"><strong>{{ __('Theme URI: ') }}</strong><a href="{{ optional($infoData)->themeUri }}" target="_blank">{{ optional($infoData)->themeUri }}</a></p>
+                                                                    <p style="margin-bottom: 0;"><strong>{{ __('Version: ') }}</strong>{{ optional($infoData)->version }}</p>
+                                                                    <p style="margin-bottom: 0;"><strong>{{ __('Description: ') }}</strong>{{ optional($infoData)->description }}</p>
+                                                                    <p style="margin-bottom: 0;"><strong>{{ __('License: ') }}</strong>{{ optional($infoData)->license }}</p>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div class="card-footer">
-                                                            <div class="d-flex justify-content-center buttons px-1 py-2">
-                                                                @if($theme != $themeActive->option_value)
-                                                                    <form class="d-inline-block w-auto" action="{{ route('dash.themes.activate', $theme) }}" method="post">
-                                                                        @csrf
-                                                                        <button type="submit" class="btn btn-success no-filter mx-1">{{ __('activate') }}</button>
-                                                                    </form>
-                                                                    <form class="d-inline-block w-auto" action="{{ route('dash.themes.delete', $theme) }}" method="post">
-                                                                        @csrf
-                                                                        <button type="submit" class="btn btn-danger no-filter delete mx-1">{{ __('Delete') }}</button>
-                                                                    </form>
-                                                                @endif
-                                                            </div>
+                                                        <div class="card-footer p-3 d-flex justify-content-center">
+                                                            <form class="d-inline-block w-auto" action="{{ route('dash.themes.activate', $theme) }}" method="post">
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-success no-filter mx-1">{{ __('activate') }}</button>
+                                                            </form>
+                                                            <form class="d-inline-block w-auto" action="{{ route('dash.themes.delete', $theme) }}" method="post">
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-danger no-filter delete mx-1">{{ __('Delete') }}</button>
+                                                            </form>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -231,6 +327,21 @@
                 return true;
             }
             return false;
+        });
+    </script>
+    <script>
+        jQuery(document).ready(function($){
+            let openPopup = $('.open-full-info-popup'),
+                closePopup = $('.close-window-content');
+            openPopup.on('click', function(e){
+                e.preventDefault();
+                let target = $(this).data('theme');
+                $('#'+target).addClass('show');
+            });
+            closePopup.on('click', function(e){
+                e.preventDefault();
+                $('.theme-info-full').removeClass('show');
+            });
         });
     </script>
 @endsection
