2e6087d2b1e723c7aae72179255638d8625aea37
Roman A <thewitness45@gmail.com>
2024-07-07 20:10:32 +0300

    add theme

--- resources/views/front/intake-digital/templates/page-homepage.blade.php
+++ resources/views/front/intake-digital/templates/page-homepage.blade.php
@@ -4,7 +4,7 @@
             <div class="swiper homepage-slider-container">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
-                        <a class="slide-inner" href="">
+                        <a class="slide-inner" href="https://store.intakedigital.net/item/outrun-digitalism-vol-1/" target="_blank">
                             <img src="{{ getThemeAssetsUri('/assets/images/slide-01.jpg') }}" alt="Slide 01" width="1520" height="760">
                         </a>
                     </div>
