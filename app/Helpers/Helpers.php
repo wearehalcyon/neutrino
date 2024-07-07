5ff85e796372679924015adbc443230af5dbade2
Roman A <thewitness45@gmail.com>
2024-07-07 18:18:20 +0300

    add theme

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -281,8 +281,29 @@ if (!function_exists('getMenu')) {
 
             if ($container) {
                 $menuHtml = '<nav' . $menuID . ' class="menu-nav ' . $menu_class . '">';
-                $menuHtml .= '<ul class="menu-list">';
-                $menuHtml .= '';
+                $menuHtml .= '<ul' . $menuID . ' class="menu-list ' . $menu_class . '">';
+                foreach ($items as $item) {
+                    $subItems = $item->getSubItems();
+                    if ($subItems->isNotEmpty()) {
+                        $menuHtml .= '<li class="menu-item menu-item-' . $item->id . ' menu-item-has-children">';
+                    } else {
+                        $menuHtml .= '<li class="menu-item menu-item-' . $item->id . '">';
+                    }
+                    $menuHtml .= '<a href="' . $item->slug . '" title="' . $item->name . '">' . $item->name . '</a>';
+
+                    if ($subItems->isNotEmpty()) {
+                        $menuHtml .= '<ul class="sub-menu">';
+                        foreach ($subItems as $subItem) {
+                            $menuHtml .= '<li class="sub-menu-item sub-menu-item-' . $subItem->id . '">';
+                            $menuHtml .= '<a href="' . $subItem->slug . '" title="' . $subItem->name . '">' . $subItem->name . '</a>';
+                            $menuHtml .= '</li>';
+                        }
+                        $menuHtml .= '</ul>';
+                    }
+
+                    $menuHtml .= '</li>';
+                }
+                $menuHtml .= '</ul>';
                 $menuHtml .= '</nav>';
             } else {
                 $menuHtml = '<ul' . $menuID . ' class="menu-list ' . $menu_class . '">';
