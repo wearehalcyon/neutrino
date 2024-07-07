3c7d0fe120d27c8efbd78bb5204b4ac785553200
Roman A <thewitness45@gmail.com>
2024-07-07 20:03:40 +0300

    add theme

--- resources/views/front/intake-digital/templates/page-homepage.blade.php
+++ resources/views/front/intake-digital/templates/page-homepage.blade.php
@@ -1,5 +1,10 @@
 @include('front.intake-digital.header')
     <section class="homepage-slider">
-
+        <div class="slider-pictures">
+            123
+        </div>
+        <div class="sliser-content">
+            123
+        </div>
     </section>
 @include('front.intake-digital.footer')
