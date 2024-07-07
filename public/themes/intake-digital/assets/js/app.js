0de4d3bfd699f578a75a4d338518abf837ca124e
Roman A <thewitness45@gmail.com>
2024-07-07 20:07:33 +0300

    add theme

--- public/themes/intake-digital/assets/js/app.js
+++ public/themes/intake-digital/assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 jQuery(document).ready(function($){
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".homepage-slider-container", {
         spaceBetween: 30,
         pagination: {
             el: ".swiper-pagination",
--- resources/views/front/intake-digital/templates/page-homepage.blade.php
+++ resources/views/front/intake-digital/templates/page-homepage.blade.php
@@ -3,15 +3,11 @@
         <div class="slider-pictures">
             <div class="swiper homepage-slider-container">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
+                    <div class="swiper-slide">
+                        <div class="slide-inner">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
