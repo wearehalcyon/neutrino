96f206fabbcee3758f1baa5e6578a27a6fc52b5f
Roman A <thewitness45@gmail.com>
2024-07-09 01:49:59 +0300

    continue theme development

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -11,10 +11,48 @@
         body{
             background: #000;
             color: #fff;
+            font-family: sans-serif;
+            overflow: hidden;
+        }
+        .error-page{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            width: 100vw;
+            height: 100vh;
+        }
+        .error-page img{
+            width: 500px;
+            height: auto;
+            max-width: 100%;
+        }
+        .cta{
+            text-align: center;
+            margin-top: 50px;
+        }
+        .cta a{
+            display: inline-block;
+            text-decoration: none;
+            background-color: #FFA800;
+            color: #000;
+            border-radius: 4px;
+            padding: 15px 30px;
+            font-weight: 600;
+            text-transform: uppercase;
+        }
+        .cta a:hover{
+            background-color: #fff;
         }
     </style>
 </head>
 <body>
-
+    <main class="error-page">
+        <div>
+            <img src="{{ getThemeAssetsUri('/assets/images/error-404.webp') }}" alt="Error 404 Image">
+            <div class="cta">
+                <a href="{{ url('/') }}" title="Go to homepage">Go to Homepage</a>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
