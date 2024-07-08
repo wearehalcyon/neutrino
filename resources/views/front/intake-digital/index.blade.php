126f4051ddd32485fe92c5e4900729e6c1a93df1
Roman A <thewitness45@gmail.com>
2024-07-09 02:08:34 +0300

    continue theme development

--- resources/views/front/intake-digital/index.blade.php
+++ resources/views/front/intake-digital/index.blade.php
@@ -1,5 +1,10 @@
 @include('front.intake-digital.header')
-    <section class="inner-hero"></section>
+    <section class="inner-hero">
+        <img src="{{ getThemeAssetsUri('/assets/images/hero/hero-0' . rand(1, 9) . '.jpg') }}" alt="Internal Hero Image">
+        <div class="container">
+            <h1 class="hero-title">{{ $page->name }}</h1>
+        </div>
+    </section>
     <main class="content">
         <div class="container">
             <div class="row">
