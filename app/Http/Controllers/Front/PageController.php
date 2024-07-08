481dbca30949ffe3430f33463c853d0a32a61bbc
Roman A <thewitness45@gmail.com>
2024-07-09 01:27:16 +0300

    continue theme development

--- app/Http/Controllers/Front/PageController.php
+++ app/Http/Controllers/Front/PageController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use Illuminate\Http\Request;
+use App\Models\Page;
 
 class PageController extends Controller
 {
     public function internal($slug)
     {
-        dd($slug);
+        $page = Page::where('slug', $slug)->first();
+
+        $theme = Setting::where('option_name', 'front_theme')->first();
+        $theme = $theme->option_value;
+        $template = $page->template;
+
+        if($template != 'default') {
+            $template = 'templates.page-' . $template;
+        } else {
+            $template = 'index';
+        }
+
+        return view('front.' . $theme . '.' . $template);
     }
 }
