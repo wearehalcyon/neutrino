8787e9c8d19b57599916fe792a6755db5a3a6e91
ArtRomanWorx <thewitness45@gmail.com>
2024-07-09 14:27:25 +0300

    fix favicon issue

--- resources/views/front/intake-digital/index.blade.php
+++ resources/views/front/intake-digital/index.blade.php
@@ -8,11 +8,16 @@
     <main class="content">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 col-lg-8 col-xl-9">
+                <div class="col-md-12 col-lg-8 col-xl-8 col-xxl-9">
                     <div class="text">{!! $page->content !!}</div>
                 </div>
-                <div class="col-md-12 col-lg-4 col-xl-3">
-                    {!! getContactForm('Homepage Form', '2') !!}
+                <div class="col-md-12 col-lg-4 col-xl-4 col-xxl-3 mt-3 mt-xl-0">
+                    <aside class="sidebar">
+                        <div class="sidebar-widget">
+                            <h4 class="widget-title">Contact Us</h4>
+                            {!! getContactForm('Sidebar Form', '3') !!}
+                        </div>
+                    </aside>
                 </div>
             </div>
         </div>
