6e5f8a26767ac7e4f9318c2e4a9a720550bc6822
Roman A <thewitness45@gmail.com>
2024-07-13 14:06:57 +0300

    fix admin functionality

--- resources/views/dashboard/partials/top-header.blade.php
+++ resources/views/dashboard/partials/top-header.blade.php
@@ -20,6 +20,79 @@
     <nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
         <div class="container-fluid">
             <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
+                <li class="nav-item topbar-icon dropdown hidden-caret">
+                    <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-envelope"></i>
+                        @if(getContactFormsMessages()->count() > 0)
+                            <span class="notification">{{ getContactFormsMessages()->count() }}</span>
+                        @endif
+                    </a>
+                    <ul class="dropdown-menu messages-notif-box animated fadeIn" aria-labelledby="messageDropdown">
+                        <li>
+                            <div class="dropdown-title d-flex justify-content-between align-items-center">
+                                {{ __('Contact Form Messages') }}
+{{--                                <a href="#" class="small">Mark all as read</a>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <div class="scroll-wrapper message-notif-scroll scrollbar-outer" style="position: relative;"><div class="message-notif-scroll scrollbar-outer scroll-content" style="height: auto; margin-bottom: 0px; margin-right: 0px; max-height: 0px;">
+                                    <div class="notif-center">
+                                        <a href="#">
+                                            <div class="notif-img">
+                                                <img src="../assets/img/jm_denis.jpg" alt="Img Profile">
+                                            </div>
+                                            <div class="notif-content">
+                                                <span class="subject">Jimmy Denis</span>
+                                                <span class="block">
+															How are you ?
+														</span>
+                                                <span class="time">5 minutes ago</span>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="notif-img">
+                                                <img src="../assets/img/chadengle.jpg" alt="Img Profile">
+                                            </div>
+                                            <div class="notif-content">
+                                                <span class="subject">Chad</span>
+                                                <span class="block">
+															Ok, Thanks !
+														</span>
+                                                <span class="time">12 minutes ago</span>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="notif-img">
+                                                <img src="../assets/img/mlane.jpg" alt="Img Profile">
+                                            </div>
+                                            <div class="notif-content">
+                                                <span class="subject">Jhon Doe</span>
+                                                <span class="block">
+															Ready for the meeting today...
+														</span>
+                                                <span class="time">12 minutes ago</span>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="notif-img">
+                                                <img src="../assets/img/talha.jpg" alt="Img Profile">
+                                            </div>
+                                            <div class="notif-content">
+                                                <span class="subject">Talha</span>
+                                                <span class="block">
+															Hi, Apa Kabar ?
+														</span>
+                                                <span class="time">17 minutes ago</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div><div class="scroll-element scroll-x"><div class="scroll-element_outer"><div class="scroll-element_size"></div><div class="scroll-element_track"></div><div class="scroll-bar"></div></div></div><div class="scroll-element scroll-y"><div class="scroll-element_outer"><div class="scroll-element_size"></div><div class="scroll-element_track"></div><div class="scroll-bar"></div></div></div></div>
+                        </li>
+                        <li>
+                            <a class="see-all" href="javascript:void(0);">See all messages<i class="fa fa-angle-right"></i> </a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="nav-item open-homepage">
                     <a href="{{ route('pages.home') }}" class="nav-link no-filter" title="{{ __('Open Site') }}" target="_blank">
                         <i class="fas fa-globe"></i> {{ __('Open Site') }}
