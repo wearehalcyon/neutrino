0c699b13bea53a271fde9dbafa71bc5632253d32
unknown <thewitness45@gmail.com>
2024-07-30 00:20:54 +0300

    add applications

--- app/Http/Controllers/Dashboard/AppsController.php
+++ app/Http/Controllers/Dashboard/AppsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 class AppsController extends Controller
 {
@@ -14,6 +15,36 @@ class AppsController extends Controller
 
         $routeName = Route::currentRouteName();
 
-        return view('dashboard.page-apps', compact('routeName'));
+        $appsRoot = __DIR__ . '/../../../../app/Applications/';
+        $appsDir = glob($appsRoot . '*', GLOB_ONLYDIR);
+        
+        foreach ($appsDir as $item) {
+            $appName = basename($item);
+            include $appsRoot . $appName . '/' . $appName . '.php';
+
+            if (File::exists($appsRoot . $appName . '/' . $appName . '.php')) {
+                $filePHP = $appsRoot . $appName . '/' . $appName . '.php';
+            } else {
+                $filePHP = null;
+            }
+            if (File::exists($appsRoot . $appName . '/' . 'info.json')) {
+                $fileJSON = json_decode(File::get($appsRoot . $appName . '/' . 'info.json'));
+            } else {
+                $fileJSON = null;
+            }
+            if (File::exists($appsRoot . $appName . '/' . 'icon.svg')) {
+                $fileSVG = File::get($appsRoot . $appName . '/' . 'icon.svg');
+            } else {
+                $fileSVG = null;
+            }
+
+            $apps[] = [
+                'php' => $filePHP,
+                'json' => $fileJSON,
+                'svg' => $fileSVG,
+            ];
+        }
+
+        return view('dashboard.page-apps', compact('routeName', 'apps'));
     }
 }
--- resources/views/dashboard/page-apps.blade.php
+++ resources/views/dashboard/page-apps.blade.php
@@ -3,6 +3,13 @@
 @section('title', __('Applications'))
 
 @section('header-scripts')
+    <style>
+        .app-icon,
+        .app-icon svg{
+            width: 42px;
+            height: 42px;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -21,11 +28,36 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <div class="card-title">{{ __('Applications List') }}</div>
+                    <div class="card-title">{{ __('Installed Applications List') }}</div>
                 </div>
                 <div class="card-body">
                     <div class="row">
-
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">{{  __('Name') }}</th>
+                                    <th scope="col">{{  __('Version') }}</th>
+                                    <th scope="col">{{  __('Author') }}</th>
+                                    <th scope="col">{{  __('Description') }}</th>
+                                    <th scope="col">{{  __('Status') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($apps as $app)
+                                    @php($app = (array)$app)
+                                    @if($app['json'])
+                                        <tr>
+                                            <td class="app-icon">{!! $app['svg'] !!}</td>
+                                            <td>{{  $app['json']->name }}</td>
+                                            <td>{{  $app['json']->version }}</td>
+                                            <td>{{  $app['json']->author }}</td>
+                                            <td>{{  $app['json']->description }}</td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
