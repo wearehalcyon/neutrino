a9a8528d4531a868c5b6562bc86a9d6a25ee056e
unknown <thewitness45@gmail.com>
2024-08-02 00:33:15 +0300

    add support of action hooks / events and listeners

--- app/Applications/help-center/pages/page-index.php
+++ app/Applications/help-center/pages/page-index.php
@@ -1,1 +1,19 @@
-<?php
+<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+    <div>
+        <h3 class="fw-bold mb-3"><?php echo __('Help Center'); ?></h3>
+        <h6 class="op-7 mb-2"><?php echo __('Quick Neutrino Documentation'); ?></h6>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title"><?php echo __('Documentation List'); ?></div>
+            </div>
+            <div class="card-body">
+                In Progress...
+            </div>
+        </div>
+    </div>
+</div>
--- app/Applications/seo-support/seo-support.php
+++ app/Applications/seo-support/seo-support.php
@@ -1,1 +1,22 @@
 <?php
+// Register admin menu item in tools section
+addAction('nt_sidebar_menu_items_tools', function(){
+    if (hideAccess([2,3,4,5])) :
+?>
+    <li class="nav-item<?php echo request()->route('slug') == 'seo-support' ? ' active' : null; ?>">
+        <a href="<?php echo route('dash.tools.page', 'seo-support'); ?>">
+            <i class="fab fa-searchengin"></i>
+            <p><?php echo __('SEO Support'); ?></p>
+        </a>
+    </li>
+<?php
+    endif;
+}, 1);
+
+// Add page action
+addFilter('custom_admin_page_title', function($seo_support_title) {
+    return __('SEO Support');
+}, 1);
+addAction('custom_admin_page_content', function(){
+    require_once __DIR__ . "/pages/page-index.php";
+}, 1);
