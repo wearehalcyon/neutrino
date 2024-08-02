5fc537d2896fd6d9be45b02e89136158f970fd91
unknown <thewitness45@gmail.com>
2024-08-02 16:00:05 +0300

    add support of action hooks / events and listeners

--- app/Applications/help-center/tabs/get-theme-assets-url.php
+++ app/Applications/help-center/tabs/get-theme-assets-url.php
@@ -10,5 +10,6 @@
 <h4 style="font-size:18px;">How to use</h4>
 <p><?php echo __('To output an <code>getThemeAssetsUri</code>, use the following code anywhere in your template:'); ?></p>
 <pre class="code">
-    <?php echo htmlspecialchars('getThemeAssetsUri($suffix = \'my-theme/assets/css/style.css\')'); ?>
+    <?php echo htmlspecialchars('getThemeAssetsUri($suffix = \'/my-theme/style.css\')'); ?>
 </pre>
+<p><?php echo __('Return: '); ?><code>https://mysite.com/themes/my-theme/style.css</code></p>
