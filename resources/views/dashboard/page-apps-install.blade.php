b0337007541ea8494a8067dfab45d6f920f8d6b2
unknown <thewitness45@gmail.com>
2024-08-02 17:45:50 +0300

    add support of action hooks / events and listeners

--- resources/views/dashboard/page-apps-install.blade.php
+++ resources/views/dashboard/page-apps-install.blade.php
@@ -3,6 +3,34 @@
 @section('title', __('Install App'))
 
 @section('header-scripts')
+    <style>
+        .app-item{
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .app-item .icon{
+            width: 100px;
+            height: 100px;
+        }
+        .app-item .icon img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .app-item .app-data{
+            padding-top: 10px;
+            padding-left: 20px;
+        }
+        .app-item .app-data h6{
+            font-size: 18px;
+            font-weight: 600;
+            margin-block: 0;
+        }
+        .app-item .app-data span{
+            font-size: 14px;
+            color: #999;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -30,7 +58,19 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        123
+                        @foreach($apps as $app)
+                            <div class="col-12 col-md-12 col-lg-6 col-xl-4 col-xxl-3 mt-3 mb-3">
+                                <div class="app-item">
+                                    <div class="icon">
+                                        <img src="{{  $app->icon }}" alt="{{ $app->name . ' App Icon' }}" width="100" height="100">
+                                    </div>
+                                    <div class="app-data">
+                                        <h6>{{ $app->name }}</h6>
+                                        <span>{{ __('Version: ') . $app->version }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
